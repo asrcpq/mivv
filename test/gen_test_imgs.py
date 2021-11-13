20b45b6fea699bb92e0e516c13f4c9082b2b81b2
asrcpq <asrcpq@outlook.com>
2021-11-13 11:08:19 +0000

    add timer

--- test/gen_test_imgs.py
+++ test/gen_test_imgs.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw
-for i in range(79):
-	img = Image.new('L', (120 - i, 20 + i), color = 255)
+for i in range(4000):
+	img = Image.new('L', (500 - i // 10, 100 + i // 10), color = 255)
 	d = ImageDraw.Draw(img)
 	d.text((10, 10), str(i))
-	img.save(f"{i:03}.png")
+	img.save(f"{i:04}.png")

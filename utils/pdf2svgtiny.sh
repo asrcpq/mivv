87d08b15e1e822d0a5726c8618e964912cd57397
asrcpq <asrcpq@outlook.com>
2021-11-24 06:53:19 +0000

    replace lock_size with preserve_viewport

--- utils/pdf2svgtiny.sh
+++ utils/pdf2svgtiny.sh
@@ -5,5 +5,6 @@ cd "$1.to_svg.d"
 pdf2svg "../$1" "%d.svg" all
 for file in *.svg; do
 	geom="$(sed -n '2p' "$file" | grep -o 'width="[0-9]*pt" height="[0-9]*pt"')"
+	sed -i "s/viewBox=\"[^\"]*\" //g" "$file"
 	sed -i "s/symbol/g/g;2a <rect $geom fill=\"white\"/>" "$file"
 done

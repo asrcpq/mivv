84589938859ee39b6f79b5a9c78f01e90de9d0c6
asrcpq <asrcpq@outlook.com>
2021-12-14 03:18:18 +0000

    add nix

---
+++ default.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs>{};
+
+python3.pkgs.buildPythonApplication rec {
+	pname = "mivv";
+	version = "0.1.0";
+
+	src = ./.;
+	nativeBuildInputs = [ qt5.wrapQtAppsHook python3.pkgs.setuptools ];
+
+	propagatedBuildInputs = with python3.pkgs; [ pyqt5 qt5.qtsvg ];
+
+	dontWrapQtApps = true;
+	preFixup = ''
+			wrapQtApp $out/bin/mivv
+	'';
+}
